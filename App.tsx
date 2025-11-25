0f84b5cc791ffc036060778063236131fa3ab243
Odebisi idowu solomon <odebisiidowu09@gmail.com>
2025-11-25 15:51:00 +0100

    made some updates

--- App.tsx
+++ App.tsx
@@ -7,13 +7,18 @@ import AuthNavigator from "./src/navigations/AuthNavigator";
 import BaseNavigator from "./src/navigations/BaseNavigator";
 import NfcScreen from "./src/screens/NfcScreen";
 import { combineStore } from "./src/store";
-import { Text } from "react-native";
+import { Platform, Text, View } from "react-native";
 import "./global.css"
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export const isIosPlatform = Platform.OS === 'ios';
 
 export default function App() {
   const Stack = createStackNavigator();
 
   const { token } = combineStore();
+  const insets = useSafeAreaInsets();
+
 
 
   // return <Text>Hello</Text>
@@ -21,14 +26,24 @@ export default function App() {
   return (
     <PaperProvider>
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          {token ? (
-            <Stack.Screen name="BaseNavigator" component={BaseNavigator} />
-          ) : (
-            <Stack.Screen name="AuthNavigator" component={AuthNavigator} />
-          )}
-        </Stack.Navigator>
+        <View style={{
+          flex: 1,
+          // paddingTop: isIosPlatform ? null : insets.top,
+          paddingTop: insets.top,
+          paddingBottom: isIosPlatform ? insets.bottom / 2 : insets.bottom,
+          // paddingBottom: insets.bottom,
+          paddingLeft: isIosPlatform ? null : insets.left,
+          paddingRight: isIosPlatform ? null : insets.right,
+        }}>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            {token ? (
+              <Stack.Screen name="BaseNavigator" component={BaseNavigator} />
+            ) : (
+              <Stack.Screen name="AuthNavigator" component={AuthNavigator} />
+            )}
+          </Stack.Navigator>
+        </View>
       </NavigationContainer>
-    </PaperProvider>
+    </PaperProvider >
   );
 }
